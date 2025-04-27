52112c642127c4d7347d091f8a55a2098250c98c
Pau Colomé <paucolome10@gmail.com>
2025-04-27 12:16:17 +0200

    feat(digitalsession): last rest controllers

--- spring-2025-user/src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
+++ spring-2025-user/src/main/java/edu/uoc/epcsd/user/application/rest/DigitalSessionRESTController.java
@@ -41,28 +41,91 @@ public class DigitalSessionRESTController {
 
     @GetMapping("/{digitalSessionId}")
     public DigitalSession getDigitalSessionById(@PathVariable @NotNull Long digitalSessionId) {
-        return digitalSessionService.getDigitalSessionById(digitalSessionId).map(item -> ResponseEntity.ok().body(item))
-                .orElse(ResponseEntity.notFound().build());
+        log.trace("getDigitalSessionById");
+
+        return digitalSessionService.getDigitalSessionById(digitalSessionId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/digitalByUser"
-    // and create the method findDigitalSessionByUser(@RequestParam @NotNull Long userId)
-    // which call the corresponding findDigitalSessionByUser method 
+    // and create the method findDigitalSessionByUser(@RequestParam @NotNull Long userId)˘
+    // which call the corresponding findDigitalSessionByUser method
+
+    @GetMapping("/digitalByUser")
+    public List<DigitalSession> findDigitalSessionByUser(@RequestParam @NotNull Long userId) {
+        log.trace("findDigitalSessionByUser");
+
+        return digitalSessionService.findDigitalSessionByUser(userId);
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/createDigital"
     // and create the method createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest)
-    // which call the corresponding createDigitalSession method 
+    // which call the corresponding createDigitalSession method
+
+    @PostMapping("/createDigital")
+    public ResponseEntity<Long> createDigitalSession(@RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest) {
+        log.trace("createDigitalSession");
+
+        log.trace("Creating product {}", createDigitalSessionRequest);
+
+        DigitalSession session = DigitalSession.builder()
+                .userId(createDigitalSessionRequest.getUserId())
+                .description(createDigitalSessionRequest.getDescription())
+                .location(createDigitalSessionRequest.getLocation())
+                .link(createDigitalSessionRequest.getLink())
+                .build();
+
+        try {
+
+            Long digitalSessionId = digitalSessionService.createDigitalSession(session);
+
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{digitalSessionId}").buildAndExpand(digitalSessionId).toUri();
+
+            return ResponseEntity.created(uri).body(digitalSessionId);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/updateDigital/{digitalSessionId}"
     // and create the method updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest updateDigitalSessionRequest)
-    // which call the corresponding updateDigitalSession method 
+    // which call the corresponding updateDigitalSession method
+
+    @PutMapping("/updateDigital/{digitalSessionId}")
+    public ResponseEntity<Boolean> updateDigitalSession(@PathVariable @NotNull Long digitalSessionId, @RequestBody @Valid CreateDigitalSessionRequest createDigitalSessionRequest) {
+        log.trace("updateDigitalSession");
+
+        log.trace("Updating product {} with {}", digitalSessionId, createDigitalSessionRequest);
+
+        try {
+            digitalSessionService.updateDigitalSession(digitalSessionId, createDigitalSessionRequest.getDescription(), createDigitalSessionRequest.getLink(), createDigitalSessionRequest.getLocation(), createDigitalSessionRequest.getUserId());
+
+            return ResponseEntity.ok().body(true);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(false);
+        }
+    }
 
     // TODO: add the code for the missing system operations here: 
     // use the corresponding mapping HTTP request annotation with the parameter: "/removeDigital/{digitalSessionId}"
     // and create the method removeDigitalSession(@PathVariable @NotNull Long digitalSessionId
-    // which call the corresponding removeDigitalSession method 
+    // which call the corresponding removeDigitalSession method
+
+    @DeleteMapping("/removeDigital/{digitalSessionId}")
+    public ResponseEntity<Boolean> removeDigitalSession(@PathVariable @NotNull Long digitalSessionId) {
+        log.trace("removeDigitalSession");
+
+        log.trace("Removing product {}", digitalSessionId);
+
+        try {
+            digitalSessionService.removeDigitalSession(digitalSessionId);
+
+            return ResponseEntity.ok().body(true);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(false);
+        }
+    }
 
 }
